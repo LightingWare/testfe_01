26ebd824055d24088db9d13c60705cd20aefe3bc
LightningWare_YT <146706767+LightingWare@users.noreply.github.com>
2026-01-16 12:35:44 +1100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,18 @@
+function SendMessage(msg: string, duration: number)
+    pcall(Function()
+        game:GetService("StarterGui"):SetCore(
+            "SendNotification", 
+            {
+                Title = script.Name;
+                Text = msg;
+                Icon = "rbxassetid://00000000000"; -- icon ID
+                Duration = duration or 2
+            }
+        )
+    end)
+end
+
 print("Initializing FE")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LightingWare/testfe_01/refs/heads/main/main.lua"))()
+
+SendMessage("Initializing FE")
